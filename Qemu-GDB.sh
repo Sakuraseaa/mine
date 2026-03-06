0673528cb5ce1f6b61bee2583dfa011e7243d5c1
Sakura <1214200610@qq.com>
2026-03-07 02:41:40 +0800

    :rocket: Fixed Configuration

--- Qemu-GDB.sh
+++ Qemu-GDB.sh
@@ -14,19 +14,3 @@ qemu-system-x86_64 \
     -chardev file,id=com1,path="/home/steven/mine/log/mine.log" \
     -serial chardev:com1
 #    -chardev file,id=com1,path="/home/steven/mine/log/mine_${timestamp}.log" \
-
-# Terminal
-# qemu-system-x86_64 \
-#     -vga std \
-#     -hdc /home/steven/mine/bochs/hd.img \
-#     -fda /home/steven/mine/bochs/boot.img \
-#     -m 512M \
-#     -boot a \
-#     -s -S \
-#     -cpu Broadwell-v1 \
-#     -chardev stdio,id=serial0,mux=on \
-#     -serial chardev:serial0 \
-#     -mon chardev=serial0,mode=readline \
-
-
-
